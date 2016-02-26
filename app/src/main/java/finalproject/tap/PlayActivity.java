2a8687519a73cf14eaf3563d5ae54f388e1a2d7e
kevin yen <kyen2@ucsc.edu>
2016-02-26 00:38:18 -0800

    Added a countdown timer that I found on stackoverflow. We will make some changes to it but I just set it there so I won't forget.

--- app/src/main/java/finalproject/tap/PlayActivity.java
+++ app/src/main/java/finalproject/tap/PlayActivity.java
@@ -2,6 +2,8 @@ package finalproject.tap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 
 import android.util.Log;
@@ -11,12 +13,47 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class PlayActivity extends AppCompatActivity {
+
+
+    TextView CountDownText;
+    private static final String FORMAT = "%02d:%02d:%02d";
+    int seconds , minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_main);
+
+
+        //I got the countdown timer code from
+        //http://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
+        //I just copied and paste it for now so I won't forget where I got it.
+        //will change it a bit later.
+        CountDownText = (TextView) findViewById(R.id.countdowntimer);
+        new CountDownTimer(60000, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+
+                CountDownText.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            public void onFinish() {
+                CountDownText.setText("Game Over!");
+            }
+        }.start();
+
     }
+
+
+
+
 }
