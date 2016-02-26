321cde226ae66cf4fe76026f83e97dc253307915
kevin yen <kyen2@ucsc.edu>
2016-02-26 01:07:42 -0800

    Messed with the countdown timers a bit. I added 1 countdown to begin the game and then another one as a countdown timer for the game before it is game over!

--- app/src/main/java/finalproject/tap/PlayActivity.java
+++ app/src/main/java/finalproject/tap/PlayActivity.java
@@ -3,6 +3,7 @@ package finalproject.tap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -20,7 +21,9 @@ public class PlayActivity extends AppCompatActivity {
 
 
     TextView CountDownText;
+    TextView GameStartCountDownText;
     private static final String FORMAT = "%02d:%02d:%02d";
+    private static final String FORMAT2 = "%02d:%02d:%02d";
     int seconds , minutes;
 
     @Override
@@ -28,17 +31,53 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_main);
 
+        //I got the countdown timer code from
+        //http://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
+        //I just copied and paste it for now so I won't forget where I got it.
+        //will change it a bit later.
+        GameStartCountDownText = (TextView) findViewById(R.id.gamestartcountdown);
+        new CountDownTimer(5000, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+
+                GameStartCountDownText.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            public void onFinish() {
+                GameStartCountDownText.setText("Start Game!");
+                GameStartCountDownText.setVisibility(View.INVISIBLE);
+                gameCountDownTimer();
+
+
+
+            }
+        }.start();
+
+    }
+
+
+
+
+    public void gameCountDownTimer(){
+
 
         //I got the countdown timer code from
         //http://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
         //I just copied and paste it for now so I won't forget where I got it.
         //will change it a bit later.
         CountDownText = (TextView) findViewById(R.id.countdowntimer);
+        CountDownText.setVisibility(View.VISIBLE);
+
         new CountDownTimer(60000, 1000) { // adjust the milli seconds here
 
             public void onTick(long millisUntilFinished) {
 
-                CountDownText.setText(""+String.format(FORMAT,
+                CountDownText.setText(""+String.format(FORMAT2,
                         TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
                         TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
                                 TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
@@ -50,10 +89,6 @@ public class PlayActivity extends AppCompatActivity {
                 CountDownText.setText("Game Over!");
             }
         }.start();
-
     }
-
-
-
 
 }
