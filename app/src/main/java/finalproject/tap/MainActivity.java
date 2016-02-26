fa48f47e481e2fa706adb3b6865a855309b217dd
Joseph Ou <jou1@ucsc.edu>
2016-02-25 22:26:00 -0800

    Added image assets Added imagebuttons for image assets rearranged activity_main.xml

--- app/src/main/java/finalproject/tap/MainActivity.java
+++ app/src/main/java/finalproject/tap/MainActivity.java
@@ -1,6 +1,7 @@
 package finalproject.tap;
 
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,16 +10,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageButton playGameButton = (ImageButton) findViewById(R.id.play_button);
+        final ImageButton instructions = (ImageButton) findViewById(R.id.instruction_button);
+        final ImageButton credits = (ImageButton) findViewById(R.id.Credit);
     }
 
 
