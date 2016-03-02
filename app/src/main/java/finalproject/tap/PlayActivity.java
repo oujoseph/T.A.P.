ded8b34ef13f496e1a272267189467f7a8140a1f
kevin yen <kyen2@ucsc.edu>
2016-03-01 19:58:34 -0800

    Fixed up the random box generator a bit. Added a score on the top of the screen. I still need to fix the game over part. It is not displaying until I clicked the screen.

--- app/src/main/java/finalproject/tap/PlayActivity.java
+++ app/src/main/java/finalproject/tap/PlayActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.Html;
 import android.view.Display;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
@@ -30,11 +31,17 @@ public class PlayActivity extends AppCompatActivity {
     private static final String FORMAT = "%02d:%02d:%02d";
     private static final String FORMAT2 = "%02d:%02d:%02d";
     int seconds , minutes;
-
+    //checks to see if the game is over or not
+    private int check = 0;
+    public int game_score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_main);
+
+
+        TextView scores = (TextView) findViewById(R.id.score_view);
+        scores.setText("Score: " + game_score);
 
         ImageButton ib = (ImageButton) findViewById(R.id.green_box);
         ib.setBackgroundColor(Color.TRANSPARENT);
@@ -57,7 +64,7 @@ public class PlayActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                GameStartCountDownText.setText("Start Game!");
+                //GameStartCountDownText.setText("Start Game!");
                 GameStartCountDownText.setVisibility(View.INVISIBLE);
                 gameCountDownTimer();
 
@@ -73,6 +80,9 @@ public class PlayActivity extends AppCompatActivity {
 
     public void gameCountDownTimer(){
 
+
+        onRanBoxes(null);
+        /*
         ImageButton ib = (ImageButton) findViewById(R.id.green_box);
         Random randomizer = new Random();
         int xaxis = randomizer.nextInt(450);
@@ -82,7 +92,7 @@ public class PlayActivity extends AppCompatActivity {
 
 
         ib.setVisibility(View.VISIBLE);
-
+        */
         //I got the countdown timer code from
         //http://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
         //I just copied and paste it for now so I won't forget where I got it.
@@ -103,7 +113,9 @@ public class PlayActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                CountDownText.setText("Game Over!");
+                //CountDownText.setText("Game Over!");
+                CountDownText.setVisibility(View.INVISIBLE);
+                check = 1; //Game is over.
             }
         }.start();
     }
@@ -112,29 +124,41 @@ public class PlayActivity extends AppCompatActivity {
     //trying to implement this
     //http://stackoverflow.com/questions/31716152/how-do-i-make-my-buttons-show-up-in-random-places-until-the-button-is-pressed
     public void onRanBoxes(View v){
-        ImageButton ib = (ImageButton) findViewById(R.id.green_box);
-        Random randomizer = new Random();
 
 
-        //how i got screen size
-        //http://stackoverflow.com/questions/19028990/android-imageview-in-random-position-with-onclicklistener
-        //come back to:
-        //http://stackoverflow.com/questions/2902640/android-get-the-screen-resolution-pixels-as-integer-values
+        if(check == 0) {
 
-        Display ssize = getWindowManager().getDefaultDisplay();
-        Point screensize = new Point();
-        ssize.getSize(screensize);
-        int x = screensize.x;
-        int y = screensize.y;
-
-        int xaxis = randomizer.nextInt(1000 % x);
-        int yaxis = randomizer.nextInt(1800 % y);
-        ib.setX(xaxis);
-        ib.setY(yaxis);
+            TextView scores = (TextView) findViewById(R.id.score_view);
+            scores.setText("Score: " + game_score);
+            ImageButton ib = (ImageButton) findViewById(R.id.green_box);
+            Random randomizer = new Random();
 
 
-        ib.setVisibility(View.VISIBLE);
+            //how i got screen size
+            //http://stackoverflow.com/questions/19028990/android-imageview-in-random-position-with-onclicklistener
+            //come back to:
+            //http://stackoverflow.com/questions/2902640/android-get-the-screen-resolution-pixels-as-integer-values
 
+            Display ssize = getWindowManager().getDefaultDisplay();
+            Point screensize = new Point();
+            ssize.getSize(screensize);
+            int x = screensize.x;
+            int y = screensize.y;
+
+            int xaxis = randomizer.nextInt(x - 100) + 50;
+            int yaxis = randomizer.nextInt(y - 50) + 5;
+            ib.setX(xaxis);
+            ib.setY(yaxis);
+
+
+            ib.setVisibility(View.VISIBLE);
+            game_score = game_score + 1;
+
+        }else {
+            GameStartCountDownText = (TextView) findViewById(R.id.gamestartcountdown);
+            GameStartCountDownText.setText(Html.fromHtml("<p>Game Over!</p>" + "<p>Final Score: </p>" + game_score));
+            GameStartCountDownText.setVisibility(View.VISIBLE);
+        }
 
     }
 
