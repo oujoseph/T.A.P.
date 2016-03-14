9221757aa758f04cebcac21a039707c481acbd09
Joseph Ou <jou1@ucsc.edu>
2016-03-14 12:09:44 -0700

    wtf

--- app/src/main/java/finalproject/tap/SettingsActivity.java
+++ app/src/main/java/finalproject/tap/SettingsActivity.java
@@ -14,36 +14,14 @@ import com.facebook.share.widget.ShareDialog;
 
 
 public class SettingsActivity extends AppCompatActivity {
-    CallbackManager callbackManager;
-    ShareDialog shareDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_settings);
 
 
-
-        callbackManager = CallbackManager.Factory.create();
-        shareDialog = new ShareDialog(this);
-        // this part is optional
-
-        if (ShareDialog.canShow(ShareLinkContent.class)) {
-            ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                    .setContentTitle("Hello Facebook")
-                    .setContentDescription(
-                            "The 'Hello Facebook' sample  showcases simple Facebook integration")
-                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
-                    .build();
-
-            shareDialog.show(linkContent);
-        }
     }
 
-    @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
 
     public void backMenu(View v) {
         Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
